0592461a5bf10d4efca102906935eb4d6bc1aafb
mdrishti <49712124+mdrishti@users.noreply.github.com>
2024-05-13 16:36:30 +0200

    Updates to figures and duckdb input

--- run_duckdb.sh
+++ run_duckdb.sh
@@ -1,3 +1,3 @@
 #If necessary, update the files in 'data/trydbtemp_Ontop' folder and then run the following command
-duckdb data/Ontop_input.db -c "IMPORT DATABASE 'data/trydbtemp_Ontop'"
+duckdb data/duckdb_input.db -c "IMPORT DATABASE 'data/duckdb_input'"
 
